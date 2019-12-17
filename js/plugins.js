01bd917c7954775160d62cf50c7e6196c7771c97
Joe <joe@sitejacket.co.uk>
2019-12-17 02:25:24 +0000

    Navbar complete

--- js/plugins.js
+++ js/plugins.js
@@ -1,11 +1,39 @@
 /*========== NAVBAR TRANSPARENT TO SOLID ==========*/
+function checkScroll() {
+    if ($(window).scrollTop() >= 300) {
+        $('.navbar').addClass('solid');
+    } else {
+        $('.navbar').removeClass('solid');
+    }
+}
 
 
 /*========== ADD SOLID CLASS TO NAVBAR WHEN TOGGLED ==========*/
+$(document).ready(function () {
+    checkScroll();
+    $(window).scroll(checkScroll);
+    $('.navbar-toggler').click(function () {
+        if ($(window).scrollTop() <=300) {
+            $('nav.navbar').toggleClass('solid-toggle');
+        }
+    });
+});
 
 
 /*========== CLOSE MOBILE MENU ON CLICK & SMOOTH SCROLL TO LINK ==========*/
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+    $('.navbar-toggler').addClass('collapsed');
+    $('#navbarResponsive').removeClass('show');
 
+    setTimeout(function() {
+        $('nav.navbar').removeClass('solid-toggle');
+    }, 300);        
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 1000);
+})
 
 /*========== BOUNCING DOWN ARROW ==========*/
 
